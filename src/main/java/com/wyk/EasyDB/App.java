e68d091a2f330d597e35defaf092481154b4cf0a
wykCN <wykwoocn@gmail.com>
2015-10-14 21:39:21 +0800

    init project...

---
+++ src/main/java/com/wyk/EasyDB/App.java
@@ -0,0 +1,13 @@
+package com.wyk.EasyDB;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
